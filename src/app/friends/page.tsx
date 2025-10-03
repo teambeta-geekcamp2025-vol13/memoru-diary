2552d33de1c54aa46c491160e4bfbdba4e6b5827
hengin-eer <hirokix0608@gmail.com>
2025-10-04 08:55:16 +0900

    ✨ #6 各ルートページを作成

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,9 +1,9 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function FriendsPage() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.title}>アプリトップ</h1>
+      <h1 className={styles.title}>友達ページ</h1>
     </div>
   );
 }
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,9 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function SettingsPage() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.title}>アプリトップ</h1>
+      <h1 className={styles.title}>設定ページ</h1>
     </div>
   );
 }
