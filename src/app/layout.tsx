6895e36e825819f76db3e6645c8a8dfa1eb8f28e
hengin-eer <hirokix0608@gmail.com>
2025-10-04 08:28:17 +0900

    ✨ #6 ボトムナビゲーションのレイアウトへの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import BottomNav from "@/components/page/BottomNav";
 import Header from "@/components/page/Header";
 
 const geistSans = Geist({
@@ -29,7 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         <main>{children}</main>
-        <footer>ボトムナビゲーション</footer>
+        <BottomNav />
       </body>
     </html>
   );
