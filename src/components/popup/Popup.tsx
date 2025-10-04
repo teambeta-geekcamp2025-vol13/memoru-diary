9a84ea0b1450ea57a14f40d2fae113903168876c
hengin-eer <hirokix0608@gmail.com>
2025-10-04 18:54:51 +0900

    ✨ #14 汎用ポップアップコンポの作成

---
+++ src/components/popup/Popup.tsx
@@ -0,0 +1,22 @@
+import styles from "./Popup.module.css";
+
+type Props = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  setClose: () => void;
+};
+
+export default function Popup({ children, isOpen, setClose }: Props) {
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.backdrop}>
+      <div className={styles.container}>
+        <section className={styles.content}>{children}</section>
+      </div>
+      <button className={styles.close_button} type="button" onClick={setClose}>
+        閉じる
+      </button>
+    </div>
+  );
+}
