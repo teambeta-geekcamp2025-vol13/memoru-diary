95819a266cb1f0b2cd51ea0ed68bb70058180e28
Yuka <ytani2004@gmail.com>
2025-10-05 02:03:06 +0900

    change_logo

--- src/components/page/Header.tsx
+++ src/components/page/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import IconLink from "../button/IconLink";
 import styles from "./Header.module.css";
 import ProgressBar from "./ProgressBar";
@@ -6,7 +7,13 @@ export default function () {
   return (
     <header className={styles.header}>
       <nav className={styles.navbar}>
-        <h1 className={styles.title}>Memoru日記</h1>
+        <Image
+          src="/logo.svg"
+          alt="Memoru日記ロゴ"
+          className={styles.logo}
+          height={40}
+          width={200}
+        />
         <IconLink size={32} iconName="ph:bell-duotone" href="/notify" />
       </nav>
       <ProgressBar allTasks={5} completedTasks={3} />
