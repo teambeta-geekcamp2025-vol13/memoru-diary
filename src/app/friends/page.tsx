bbc50ef47efe3aa7f089feee07ef710058a11c77
Yuka <ytani2004@gmail.com>
2025-10-05 14:01:07 +0900

    friendpage

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -4,6 +4,23 @@ export default function FriendsPage() {
   return (
     <div className={styles.page}>
       <h1 className={styles.title}>友達ページ</h1>
+      <div>
+        <h2>フレンド検索</h2>
+        <div>
+          <input type="text" />
+        </div>
+      </div>
+      <div>
+        <h2>フレンドリスト</h2>
+        <ul>
+          <li>友達1</li>
+          <li>友達2</li>
+          <li>友達3</li>
+        </ul>
+      </div>
+      <div>
+        <h2>最新の投稿</h2>
+      </div>
     </div>
   );
 }
